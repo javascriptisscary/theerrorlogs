39deb00f85ce8b9917fdf5e41dbcecc37a180745
Kenneth <crazedpokerpro@yahoo.com>
2016-03-24 12:14:33 -0400

    content preview on profile table

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,10 @@
 module PostsHelper
+  
+  #shorten content for title hover display
+  def content_preview(post_content)
+  
+    preview = "#{post_content[0..75]}... *click for more*"
+    return preview
+  
+  end
 end
