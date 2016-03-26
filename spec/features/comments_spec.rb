ea5ef823be261b3f17e2444ff1aea6de010e7532
Kenneth <crazedpokerpro@yahoo.com>
2016-03-26 16:10:33 -0400

    comment css, new/delete comment feature spec

---
+++ spec/features/comments_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+include Warden::Test::Helpers
+Warden.test_mode!
+
+
+describe "User makes a new comment" do
+  
+  before :each do
+   @user = create(:user, email: 'user@example.com', password: 'password', first_name: 'John', last_name: "Smith")
+   login_as(@user, :scope => :user)
+   @post = create(:post, user_id: @user.id, content: "I'm a post made for features testing. Comments shall work please! Please, please, please work." )
+  end
+
+  it "makes a new comment then deletes it" do
+    visit post_path(@post.id)
+    
+    
+     within(".comment-form") do
+      fill_in 'comment_content', with: "I'm a new random comment"
+    end
+     
+     click_button 'Post Comment'
+     expect(page).to have_content "I'm a post made for features testing. Comments shall work please! Please, please, please work."
+  
+  
+  
+  # delete comment
+    within(".comment-info") do
+     
+    
+      click_link 'Delete Comment'
+      expect(page).to_not have_content "I'm a post made for features testing. Comments shall work please! Please, please, please work."
+    end
+  
+  end
+
+
+
+
+
+end
