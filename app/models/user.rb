9a186118ed66626f2f1360efd7d1f3df8d5970b0
Kenneth <crazedpokerpro@yahoo.com>
2016-03-28 13:29:40 -0400

    user has_many comments, change post#new content size

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
          
   has_many :posts, dependent: :destroy
+  has_many :comments
   
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
